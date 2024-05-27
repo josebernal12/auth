90db3bec27af019e277ca646f095dcb0dec3c5ae
jose <jose12_bernal@hotmail.com>
2024-05-27 12:25:28 -0700

    dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express'
-// import dotenv from 'dotenv'
-// import cors from 'cors'
+import dotenv from 'dotenv'
+import cors from 'cors'
 import { connectDB } from './database/config'
 import authRouter from './routes/authRoutes'
-// dotenv.config()
+dotenv.config()
 
 const app = express()
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 connectDB()
 app.use('/api/auth', authRouter)
