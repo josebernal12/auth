c231480c130c4a2e8cbbc88aa5caa4c4b3f81328
jose bernal <jose12_bernal@hotmail.com>
2024-06-05 11:46:34 -0700

    express validator added

--- src/routes/auth/authRoutes.ts
+++ src/routes/auth/authRoutes.ts
@@ -1,21 +1,10 @@
 import { Router } from 'express'
 import { AuthController } from '../../controllers/auth/AuthController'
-// import { body } from 'express-validator'
+import validateCreate from '../../validators/users'
 const router = Router()
 //TODO agregar el middleware handleinputErrors
 router.post('/create-account',
-  // body('name')
-  //   .notEmpty().withMessage('el nombre no puede ir vacio'),
-  // body('password')
-  //   .notEmpty().withMessage('el nombre no puede ir vacio'),
-  // body('password_confirmation').custom((value, { req }) => {
-  //   if(value !== req.body.password) {
-  //     throw new Error('Los Password no son iguales')
-  //   }
-  //   return true
-  // }),
-  // body('email')
-  //   .isEmail().withMessage('el email no es valido'),
+  validateCreate,
   AuthController.createAccount
 )
 router.post('/login', AuthController.login)
