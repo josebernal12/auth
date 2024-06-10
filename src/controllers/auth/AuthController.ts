94e21c18f6d8ea4379a86192f541369b066c23de
jose bernal <jose12_bernal@hotmail.com>
2024-06-10 10:52:36 -0700

    response

--- src/controllers/auth/AuthController.ts
+++ src/controllers/auth/AuthController.ts
@@ -5,13 +5,13 @@ export class AuthController {
 
   static createAccount = async (req: Request, res: Response) => {
     const { name, email, password }: UserType = req.body
-    const newAccount = await AuthService.createUser(name, email, password)
+    const response = await AuthService.createUser(name, email, password)
     res.json({
-      error: newAccount.error,
-      message: newAccount.message,
-      data: newAccount.data,
-      token: newAccount.token,
-      status: newAccount.status
+      error: response.error,
+      message: response.message,
+      data: response.data,
+      token: response.token,
+      status: response.status
     })
   }
 
